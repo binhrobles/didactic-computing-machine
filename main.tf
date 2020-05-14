afa9711ed5502d30c2465f514aeb72a6bfa409f3
Binh 🌱 <binhrobles@gmail.com>
2020-05-14 11:41:16 -0700

    proper reference

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_elastic_beanstalk_application" "didactic-eb" {
 
 resource "aws_elastic_beanstalk_environment" "didactic-eb-env" {
   name                = "DidacticComputingMachine-env-1"
-  application         = "aws_elastic_beanstalk_application.didactic-eb.name"
+  application         = aws_elastic_beanstalk_application.didactic-eb.name
   solution_stack_name = "64bit Amazon Linux 2018.03 v2.20.2 running Multi-container Docker 19.03.6-ce (Generic)"
 }
 
